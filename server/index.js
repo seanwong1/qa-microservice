e0fbb994ef5b717e80b24c310e5d5284fbca5aa7
Sean Wong <sean.uuong@gmail.com>
2023-04-18 22:09:30 -0700

    Test that postgres is returning data

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+
+const client = require('../database/SQL').client;
 const config = require("../config.js");
 
 const app = express();
 
 app.use(express.json());
 
+client.connect();
+client.query('SELECT * FROM answers LIMIT 10', (err, res) => {
+  if (err) {
+    console.log('err', err);
+  } else {
+    console.log('res', res);
+  }
+  client.end();
+});
 app.listen(config.port, () => {
   console.log("Server listening on port", config.port);
 });
