966643187d20a80f2bfd2b4508bebb2fdaf486f8
Sean Wong <sean.uuong@gmail.com>
2023-04-20 22:57:59 -0700

    Make progress on routes

--- server/index.js
+++ server/index.js
@@ -7,15 +7,53 @@ const app = express();
 
 app.use(express.json());
 
-client.connect();
-client.query('SELECT * FROM answers LIMIT 10', (err, res) => {
-  if (err) {
-    console.log('err', err);
-  } else {
-    console.log('res', res);
+app.use('/qa/questions/:question_id/answers', (req, res) => {
+  var question_id = req.params.question_id;
+  if (!req.query.page) {
+    req.query.page = 1;
   }
-  client.end();
+  if (!req.query.count) {
+    req.query.count = 5;
+  }
+  client.connect();
+  client.query('SELECT * FROM answers WHERE question_id = $1', [question_id])
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(404);
+    })
+    .then(() => {
+      client.end();
+    });
+})
+
+app.use('/qa/questions', (req, res) => {
+  if (!req.query.page) {
+    req.query.page = 1;
+  }
+  if (!req.query.count) {
+    req.query.count = 5;
+  }
+  if (!req.query.product_id) {
+    res.end('Enter product id');
+  } else {
+    client.connect();
+    client.query('SELECT * FROM questions WHERE product_id = $1', [req.query.product_id])
+      .then((result) => {
+        res.send(result.rows);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(404);
+      })
+      .then(() => {
+        client.end();
+      });
+  }
 });
+
 app.listen(config.port, () => {
   console.log("Server listening on port", config.port);
 });
