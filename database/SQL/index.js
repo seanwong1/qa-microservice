02d25e71edbb8b69e07f585eb7d8a2dcb32fb494
Sean Wong <sean.uuong@gmail.com>
2023-05-16 20:32:27 -0700

    Modify for deployment

--- database/SQL/index.js
+++ database/SQL/index.js
@@ -2,7 +2,7 @@ const { Pool, Client } = require('pg');
 
 const client = new Client({
   user: 'postgres',
-  host: 'localhost', // use db if connecting with docker
+  host: 'db', // use db if connecting with docker // use localhost if testing locally
   database: 'sdc',
   password: 'mysecretpassword',
   port: 5432,
